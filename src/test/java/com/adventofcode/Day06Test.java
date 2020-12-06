ae6c895b934d5fce711d474da6a9ac43357d3fbe
ZongoForSpeed <13649539+ZongoForSpeed@users.noreply.github.com>
2020-12-06 22:16:35 +0100

    Update Day06Test.java

--- src/test/java/com/adventofcode/Day06Test.java
+++ src/test/java/com/adventofcode/Day06Test.java
@@ -193,7 +193,8 @@ public class Day06Test {
      * <p>
      * In this example, the sum of these counts is 3 + 0 + 1 + 1 + 1 = 6.
      * <p>
-     * For each group, count the number of questions to which everyone answered "yes". What is the sum of those counts?
+     * For each group, count the number of questions to which everyone answered
+     * "yes". What is the sum of those counts?
      */
     @Test
     void inputCustomCustoms() throws IOException {
